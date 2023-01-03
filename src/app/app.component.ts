f09f75fce18b75941f44bdf39522501b06bfaf68
Kübra Ergül Köse <kuebra.erguel.koese@stecher-automation.de>
2023-01-03 04:53:13 -0800

    typescipt class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,59 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularProject';
 }
+
+
+export class Product {
+
+  //class lar üç ana bileşenden oluşur; fields, constructor and methods 
+
+  //Fields 
+
+  name:string;
+  price:number;
+  category: string;
+  isPublished: string;
+
+  //constructor
+
+  constructor(name:string, price:number, category:string, isPublished:string) {
+    this.name = name;
+    this.price = price;
+    this.category = category;
+    this.isPublished = isPublished;
+
+
+  }
+
+  //method 
+
+topla (a1:number, a2:number) {
+    console.log(a1+a2);
+    return a1 + a2 ;
+  }
+
+
+
+}
+
+///MİRAS ALMA////
+class product1 extends Product {
+  //product clasının değişkenlerini ve methodlarını kullanabiliriz.
+  //constructor kullanıcaksak, miras aldığımız constructor almamız lazım. super () ifadesiyle alabiliriz.
+
+  constructor (name: string, price: number, category: string, isPublished: string) {
+    super(name, price, category, isPublished)
+  }
+}
+
+
+/* Erişim Belirleyiciler -----------------------------------
+private, public, protected
+private dediğimizde sadece privateın tanımlandığı class lardan erişilebilir.
+protected sadece miras aldığımız classlarda kullanabiliriz.  
+nesne oluşturduğumuzda protected alanlara ulaşamayız  */
+
+console.log("ahjsdjg");
+var p = new Product("name",200,"cat","treu");
+p.topla(5,3);
+
